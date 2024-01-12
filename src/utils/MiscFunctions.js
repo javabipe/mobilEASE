dc7a643b540887a9fbdf9456c86f5ada11d7c792
javabipe <67403018+javabipe@users.noreply.github.com>
2024-01-12 13:45:10 -0300

    Update MiscFunctions.js

--- src/utils/MiscFunctions.js
+++ src/utils/MiscFunctions.js
@@ -10,6 +10,7 @@ export const identifyLocation = async () => {
     const response = await axios.get(
       `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${position.coords.latitude}&longitude=${position.coords.longitude}&localityLanguage=en`
     );
+    //https://maps.googleapis.com/maps/api/geocode/json?latlng=-18.897861931344266,-48.26394048922073&key=AIzaSyCrFDczBiXvDRY5pOTNYHl_6gDN83SOUcE
     const location = response.data.locality + "," + response.data.city;
     console.log(location);
     return {
