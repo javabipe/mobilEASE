8e3641dcdc6b0fc209f494278a8795c4373c1460
GustavoLennyng <gustavolennyng@outlook.com>
2024-02-29 17:25:56 -0300

    t

--- src/utils/server.js
+++ src/utils/server.js
@@ -2,34 +2,44 @@ import express from "express";
 import nodemailer from "nodemailer";
 import dotenv from "dotenv";
 import bodyParser from "body-parser";
-import cors from "cors"; // Adicione esta linha
+import cors from "cors";
 
 dotenv.config();
 const app = express();
 const port = 3000;
 
-app.use(cors({ origin: '*' }));
+app.use(cors({
+  origin: 'https://faec.falagov.com.br',
+  exposedHeaders: ['Content-Type'],
+  credentials: true,
+}));
 const jsonParser = bodyParser.json();
-app.listen(port, () => console.log(`Server is running on port ${port}`))
+app.listen(port, () => console.log(`Server is running on port ${port}`));
 
 app.post('/send', jsonParser, (req, res) => {
-  const user = process.env.MAIL_HOST
-  const pass = process.env.MAIL_PASS
+  res.header('Access-Control-Allow-Origin', 'https://faec.falagov.com.br');
+  res.header('Access-Control-Allow-Methods', 'POST');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+  const user = process.env.MAIL_HOST;
+  const pass = process.env.MAIL_PASS;
+
   const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
     port: 587,
-    auth: { user, pass }
-  })
+    auth: { user, pass },
+    tls: {
+      // Adicionando esta linha para permitir credenciais
+      rejectUnauthorized: false,
+    },
+  });
 
   transporter.sendMail({
     from: user,
     to: req.body.to,
     replyTo: user,
     subject: req.body.subject,
-    html: req.body.html
-  }).then(info =>
-    res.send(info)
-  ).catch(error => {
-    res.send(error)
-  });
+    html: req.body.html,
+  }).then(info => res.send(info))
+    .catch(error => res.send(error));
 });
