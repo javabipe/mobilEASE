3eff63c26b7e674b4411c4c142c4cbf3d936637c
GustavoLennyng <gustavolennyng@outlook.com>
2024-03-04 15:30:36 -0300

    Headers vercel.json

--- src/utils/FirebaseFunctions.js
+++ src/utils/FirebaseFunctions.js
@@ -136,10 +136,6 @@ export const createComplaint = async (formData, media, selectedEmails) => {
       headers: {
         Authorization: `Bearer ${sendGridApiKey}`,
         'Content-Type': 'application/json',
-        "key": "Access-Control-Allow-Credentials", "value": "true",
-        "key": "Access-Control-Allow-Origin", "value": "https://faec.falagov.com.br",
-        "key": "Access-Control-Allow-Methods", "value": "GET,OPTIONS,PATCH,DELETE,POST,PUT",
-        "key": "Access-Control-Allow-Headers", "value": "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
       },
     });
 
@@ -173,10 +169,6 @@ export const createComplaint = async (formData, media, selectedEmails) => {
       headers: {
         Authorization: `Bearer ${sendGridApiKey}`,
         'Content-Type': 'application/json',
-        "key": "Access-Control-Allow-Credentials", "value": "true",
-        "key": "Access-Control-Allow-Origin", "value": "https://faec.falagov.com.br",
-        "key": "Access-Control-Allow-Methods", "value": "GET,OPTIONS,PATCH,DELETE,POST,PUT",
-        "key": "Access-Control-Allow-Headers", "value": "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
       },
     });
 
